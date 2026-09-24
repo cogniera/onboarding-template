39d5bde4a748103fd37ccc9172aa959d592269e0
cogniera <paarths376@gmail.com>
2026-09-23 21:06:52 -0400

    prgma simd bug fix

--- src/submission.hpp
+++ src/submission.hpp
@@ -1,4 +1,4 @@
-#pragma omp simd
+#pragma once
 
 #include <cstddef>
 #include <vector>
@@ -59,7 +59,8 @@ void apply_stencil(const Grid& old_grid, Grid& new_grid){
 
   //have to make every other cell inside the grid use the formula of dissipation 
   for (std::size_t i = 1; i < old_grid.height()-1 ; i++)
-  {
+  { 
+    #pragma omp simd
     for (std::size_t j = 1; j < old_grid.width()-1 ; j++)
     {
       new_grid(i,j) = 0.5 * old_grid(i,j) + 
